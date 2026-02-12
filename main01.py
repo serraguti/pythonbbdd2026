c3bdfd76cbfcfea48ed9fc56748e1b4ff8fbff7b
serraguti <pacoserranox@gmail.com>
2026-02-12 20:24:16 +0100

    first commit

---
+++ main01.py
@@ -0,0 +1,4 @@
+from services import service01prueba
+print("Soy un Main")
+texto = service01prueba.getSaludo()
+print(texto)
---
+++ services/service01prueba.py
@@ -0,0 +1,4 @@
+#Vamos a tener un simple metodo
+#para llamarlo desde el main
+def getSaludo():
+    return "Hoy es juernes"
